9ff7150759519c85c7c19e9e113ee8fee86c2d28
CTTD-STD <58200700+CTTD-STD@users.noreply.github.com>
2020-05-17 14:08:16 +0700

    Add files via upload

---
+++ homepageForm.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<header>
+			<div class="logo">LOGO</div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Menu</a></li>
+					<li><a href="#">Online Order</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+
+		</header>
+	</body>
+</html>
