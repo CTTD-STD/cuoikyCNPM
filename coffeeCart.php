fa2a026e174ed8300dab6a27662a7e7a000d3d45
TuanKhai2512 <59407751+TuanKhai2512@users.noreply.github.com>
2020-05-24 23:10:37 +0700

    Add files via upload

--- coffeeCart.php
+++ coffeeCart.php
@@ -55,7 +55,6 @@
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
 						<span class="sr-only">Menu</span>
-						<span class="glyphicon glyphicon-menu-hamburger"></span>
 						</button>
 						<a class="navbar-brand">Đơn Nước</a>
 					</div>
@@ -64,7 +63,7 @@
 						<ul class="nav navbar-nav">
 							<li>
 								<a id="cart-popover" class="btn" data-placement="bottom" title="Shopping Cart">
-									<span class="glyphicon glyphicon-shopping-cart"></span>
+									
 									<span class="badge"></span>
 									<span class="total_price">VND 0.00</span>
 								</a>
@@ -78,10 +77,10 @@
 				<span id="cart_details"></span>
 				<div align="right">
 					<a href="#" class="btn btn-primary" id="check_out_cart">
-					<span class="glyphicon glyphicon-shopping-cart"></span> Check out
+					 Check out
 					</a>
 					<a href="#" class="btn btn-default" id="clear_cart">
-					<span class="glyphicon glyphicon-trash"></span> Clear
+					 Clear
 					</a>
 				</div>
 			</div>
